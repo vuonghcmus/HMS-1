b213e63ea14b2c8d1b81f99a40622a2ca9cf7cdb
vuonghcmus <76389096+vuonghcmus@users.noreply.github.com>
2021-11-20 15:28:38 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+const Handlebars = require("handlebars");
+const path = require("path");
+
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/about-us", function (req, res) {
+  res.render("about-us");
+});
+
+app.get("/blog", function (req, res) {
+  res.render("blog");
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact");
+});
+
+app.get("/elements", function (req, res) {
+  res.render("elements");
+});
+
+app.get("/room", function (req, res) {
+  res.render("room");
+});
+
+app.get("/services", function (req, res) {
+  res.render("services");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
