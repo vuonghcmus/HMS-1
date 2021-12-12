37be9af95e2b1377b9a3f89d3678e282a641d27c
anhvinhphan659 <anhvinhphan659@gmail.com>
2021-12-12 09:14:14 +0700

    update UI for Room-detail

--- Customer/app.js
+++ Customer/app.js
@@ -4,7 +4,7 @@ const Handlebars = require("handlebars");
 const path = require("path");
 
 const {
-  allowInsecurePrototypeAccess,
+    allowInsecurePrototypeAccess,
 } = require("@handlebars/allow-prototype-access");
 
 const app = express();
@@ -12,47 +12,49 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.engine(
-  "hbs",
-  engine({
-    extname: ".hbs",
-    defaultLayout: "main",
-    handlebars: allowInsecurePrototypeAccess(Handlebars),
-  })
+    "hbs",
+    engine({
+        extname: ".hbs",
+        defaultLayout: "main",
+        handlebars: allowInsecurePrototypeAccess(Handlebars),
+    })
 );
 app.set("view engine", "hbs");
 app.set("views", "./views");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("/", function (req, res) {
-  res.render("home");
+app.get("/", function(req, res) {
+    res.render("home");
 });
 
-app.get("/about-us", function (req, res) {
-  res.render("about-us");
+app.get("/about-us", function(req, res) {
+    res.render("about-us");
 });
 
-app.get("/blog", function (req, res) {
-  res.render("blog");
+app.get("/blog", function(req, res) {
+    res.render("blog");
 });
 
-app.get("/contact", function (req, res) {
-  res.render("contact");
+app.get("/contact", function(req, res) {
+    res.render("contact");
 });
 
-app.get("/elements", function (req, res) {
-  res.render("elements");
+app.get("/elements", function(req, res) {
+    res.render("elements");
 });
 
-app.get("/rooms", function (req, res) {
-  res.render("rooms");
+app.get("/rooms", function(req, res) {
+    res.render("rooms");
 });
-
-app.get("/services", function (req, res) {
-  res.render("services");
+app.get("/rooms/room-detail", function(req, res) {
+    res.render("room-detail");
+});
+app.get("/services", function(req, res) {
+    res.render("services");
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+    console.log(`App listening on port ${PORT}`);
 });
