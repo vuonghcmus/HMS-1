a895c04297437a0b33d1642e663d1903b1bc997f
Nguyen Hoang Tien <nguyenhoangtien100501@gmail.com>
2021-12-15 15:19:39 +0700

    update rooms page and room details

--- Customer/app.js
+++ Customer/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const Handlebars = require("handlebars");
 const path = require("path");
+const methodOverride = require('method-override')
 
 const {
     allowInsecurePrototypeAccess,
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.engine(
     "hbs",
     engine({
