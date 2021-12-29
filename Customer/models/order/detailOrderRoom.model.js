90c4588fe16b09d1b86f789f4609f360b54aa9b0
Nguyen Hoang Tien <nguyenhoangtien100501@gmail.com>
2021-12-30 00:15:47 +0700

    fix conflict

--- Customer/models/order/detailOrderRoom.model.js
+++ Customer/models/order/detailOrderRoom.model.js
@@ -4,15 +4,11 @@ const DetailOrderRoomSchema = new Schema({
     roomID: { type: Schema.Types.ObjectId },
     dateOfCheckIn: { type: Schema.Types.Date },
     dateOfCheckOut: { type: Schema.Types.Date },
-<<<<<<< HEAD
-    _id: { type: Schema.Types.ObjectId }
-}, {
-=======
+    _id: { type: Schema.Types.ObjectId },
     detailOrderService: [{ type: Schema.Types.ObjectId }],
     price: {type: Schema.Types.Number},
   },
   {
->>>>>>> 9132fac22c92e6a9aa6633f9e78d957e9ecc10e6
     collection: "DetailOrderRoom",
 });
 const DetailOrderRoom = mongoose.model(
