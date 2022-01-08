1a82f2eeabd2df14b42a6b25f736ae5092ca7ed5
vuonghcmus <76389096+vuonghcmus@users.noreply.github.com>
2022-01-08 23:49:40 +0700

    add sms verification

--- Customer/app.js
+++ Customer/app.js
@@ -2,26 +2,27 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const Handlebars = require("handlebars");
 const path = require("path");
-const methodOverride = require('method-override')
+const methodOverride = require("method-override");
 const databaseService = require("./services/database.service");
-const helpers = require('./helpers/viewEngine')
+const helpers = require("./helpers/viewEngine");
 const mongoose = require("mongoose");
+const express_handlebars_sections = require("express-handlebars-sections");
 // mongoose.connect(process.env.da)
 
 const logger = require("morgan");
 
-const routes = require('./routes/index')
+const routes = require("./routes/index");
 
 const {
-    allowInsecurePrototypeAccess,
+  allowInsecurePrototypeAccess,
 } = require("@handlebars/allow-prototype-access");
 
-databaseService.connectDatabase()
+databaseService.connectDatabase();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 // use logger and use read json , static file
 // app.use(logger("dev"));
@@ -31,24 +32,22 @@ require("./middlewares/passport")(app);
 app.use(require("./middlewares/locals"));
 
 app.engine(
-    "hbs",
-    engine({
-        extname: ".hbs",
-        defaultLayout: "main",
-        handlebars: allowInsecurePrototypeAccess(Handlebars),
-        helpers: helpers
-    })
+  "hbs",
+  engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+    helpers: helpers,
+  })
 );
 app.set("view engine", "hbs");
 app.set("views", "./views");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
-routes(app)
-
-
+routes(app);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
--- Customer/routes/customer.route.js
+++ Customer/routes/customer.route.js
@@ -10,9 +10,10 @@ router.get("/sign-in", customerController.show);
 router.post("/sign-in", customerController.login);
 router.get("/forgot-password", customerController.getForgotPassword);
 router.post("/forgot-password", customerController.postForgotPassword);
+router.post("/verification", customerController.checkVerification);
 router.get("/logout", customerController.logout);
-router.get("/profile",auth, customerController.profile);
-router.get("/change-password",auth, customerController.getChangePassword);
-router.post("/change-password",auth, customerController.postChangePassword);
+router.get("/profile", auth, customerController.profile);
+router.get("/change-password", auth, customerController.getChangePassword);
+router.post("/change-password", auth, customerController.postChangePassword);
 
-module.exports = router
+module.exports = router;
