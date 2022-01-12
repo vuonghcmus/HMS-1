d0980cc6f4f1d5656f544e0ec4fa8b8606d61f18
Nguyen Hoang Tien <nguyenhoangtien100501@gmail.com>
2022-01-13 00:53:19 +0700

    add errorHandler middleware

--- Customer/routes/index.js
+++ Customer/routes/index.js
@@ -5,19 +5,17 @@ const billRouter = require('./bill.route')
 const orderRouter = require('./order.route')
 const homeRouter = require('./home.route')
 const apiRouter = require("./api.route");
+const errorHandler = require('../middlewares/errorHandler')
+
 const routes = app => {
     app.use('/account', customerRouter)
     app.use('/services', serviceRouter)
     app.use('/rooms', roomRouter)
     app.use('/my-bill', billRouter)
     app.use('/orders', orderRouter)
-    app.use("/api", apiRouter);
+    app.use("/api", apiRouter)
     app.use('/', homeRouter)
-    app.use((req, res, next) => {
-        res.render('error/404', {
-            layout: 'main_no_head'
-        })
-    })
+    errorHandler(app)
 }
 
 module.exports = routes
