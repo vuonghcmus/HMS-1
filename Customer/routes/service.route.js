ef1a1bd1e45010db9f7e735105fa06d777000df3
anhvinhphan659 <anhvinhphan659@gmail.com>
2022-01-14 09:41:47 +0700

    fix error confirm on url

--- Customer/routes/service.route.js
+++ Customer/routes/service.route.js
@@ -5,6 +5,7 @@ const serviceController = require("../controllers/service.controller.js");
 
 router.post("/add-to-cart", serviceController.addServiceToCart);
 router.post("/update-cart/:serviceID/:qty", serviceController.updateIncart);
+router.get("/confirm", serviceController.getConfirm);
 router.post("/confirm", serviceController.confirmCart);
 router.post("/clear-cart", serviceController.clearCart);
 router.get("/:id_service", serviceController.ServiceDetail);
